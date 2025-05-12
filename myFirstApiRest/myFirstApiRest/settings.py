e8f254ebf5076c715dc9abb61aaf018b273b9928
Claudia <cgbirichinaga@alu.comillas.edu>
2025-05-12 21:59:49 +0200

    error database_url solucionado

--- myFirstApiRest/myFirstApiRest/settings.py
+++ myFirstApiRest/myFirstApiRest/settings.py
@@ -93,9 +93,14 @@ WSGI_APPLICATION = 'myFirstApiRest.wsgi.application'
 #     }
 # }
 load_dotenv()
+# DATABASES = {
+#     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
+#     }
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

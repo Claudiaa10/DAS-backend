2713452739d914df98f2840290e685060efdbc63
Claudia <cgbirichinaga@alu.comillas.edu>
2025-05-15 18:22:54 +0200

    s

--- myFirstApiRest/myFirstApiRest/settings.py
+++ myFirstApiRest/myFirstApiRest/settings.py
@@ -98,12 +98,10 @@ WSGI_APPLICATION = 'myFirstApiRest.wsgi.application'
 #     }
 
 load_dotenv()
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-    )
+'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
